edf99c89f5a5ca19f324e4c48995356ea7f2671d
Adowalma <adowalmaadao@gmail.com>
2024-12-29 21:24:29 +0100

    #01 - Active sidebar, Home, Customer index

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layouts.app');
+    return view('home');
 });
+
+Route::get('/customers',[App\Http\Controllers\CustomersController::class, 'index']);
